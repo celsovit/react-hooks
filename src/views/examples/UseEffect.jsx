29f3145957e96017f333d149b49a5a46ebe7aac4
Celso Vitorino <vitorino.celso@gmail.com>
2024-03-19 03:13:18 -0300

    Par ou Ímpar utilizando UseEffect()

--- src/views/examples/UseEffect.jsx
+++ src/views/examples/UseEffect.jsx
@@ -1,11 +1,14 @@
 import { useEffect, useState } from 'react'
+
 import PageTitle from '../../components/layout/PageTitle'
+import SectionTitle from '../../components/layout/SectionTitle'
 
 const calculateFactorial = (n) => {
     const num = parseInt(n)
 
     if (num < 0) 
-        throw new Error('Não existe fatorial de número negativo')
+        //throw new Error('Não existe fatorial de número negativo')
+        return -1
 
     if (num === 0 || num === 1) 
         return 1
@@ -17,6 +20,7 @@ const UseEffect = (props) => {
 
     const [ number, setNumber ] = useState(1)
     const [ factorial, setFactorial ] = useState(1)
+    const [ status, setStatus ] = useState('Par')
 
     useEffect(
         () => setFactorial(calculateFactorial(number)), 
@@ -29,6 +33,11 @@ const UseEffect = (props) => {
         }
     }, [factorial])
 
+    useEffect(
+        () => setStatus(number % 2 === 0 ? 'Par' : 'Ímpar'), 
+        [number]
+    )
+    
     return (
         <div className="UseEffect">
             <PageTitle
@@ -36,17 +45,25 @@ const UseEffect = (props) => {
                 subtitle="Permite executar efeitos colaterais em componentes funcionais!"
             />
 
+            <SectionTitle title="Exercício #01" />
             <div className="center">
                 <div>
                     <span className="text">(Fatorial) { number }! = </span>
-                    <span className="text red">{ factorial }</span>
+                    <span className="text red">{ factorial < 0 ? 'Erro' : factorial }</span>
                 </div>
                 <input className='input'
-                    min={ 0 }
                     type="number" 
                     value={ number } 
                     onChange={(e) => setNumber(e.target.value)}
                 />
+            </div>
+
+            <SectionTitle title="Exercício #02" />
+            <div className="center">
+                <div>
+                    <span className="text">Status: </span>
+                    <span className="text red">{ status }</span>
+                </div>
             </div>
 
         </div>
