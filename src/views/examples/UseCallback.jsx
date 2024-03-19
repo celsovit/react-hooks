826f410ec7f3b58a545bffd747bade98c190bb4d
Celso Vitorino <vitorino.celso@gmail.com>
2024-03-18 23:23:19 -0300

    Estrutura inicial

---
+++ src/views/examples/UseCallback.jsx
@@ -0,0 +1,14 @@
+import PageTitle from '../../components/layout/PageTitle'
+
+const UseCallback = (props) => {
+    return (
+        <div className="UseCallback">
+            <PageTitle
+                title="Hook UseCallback"
+                subtitle="Retorna uma função memoizada!"
+            />
+        </div>
+    )
+}
+
+export default UseCallback
