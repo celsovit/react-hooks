81c6120bb4305b8dcf1cb3d503786dd92c11fb1b
Celso Vitorino <vitorino.celso@gmail.com>
2024-03-23 18:52:28 -0300

    Uso de Context API de forma encapsulada dentro de um componente (Store)

--- src/views/App.jsx
+++ src/views/App.jsx
@@ -1,28 +1,30 @@
 import React, { useState } from 'react'
 import { BrowserRouter as Router } from  'react-router-dom'
 
-import { DataContext, data } from '../data/DataContext'
-
-import './App.css'
-
 import Menu from '../components/layout/Menu'
 import Content from '../components/layout/Content'
+import { DataContext, data } from '../data/DataContext'
+import Store from '../data/Store'
+
+import './App.css'
 
 const App = props => {
 
     const [ state, setState ] = useState(data)
 
     return (
-        <DataContext.Provider value={{
-            state: state, setState: setState
-         }}>
-            <div className="App">
-                <Router>
-                    <Menu />
-                    <Content />
-                </Router>
-            </div>
-        </DataContext.Provider>
+        <Store>
+            <DataContext.Provider value={{
+                state: state, setState: setState
+            }}>
+                <div className="App">
+                    <Router>
+                        <Menu />
+                        <Content />
+                    </Router>
+                </div>
+            </DataContext.Provider>
+        </Store>
     )
 }
 
--- src/views/examples/UseContext.jsx
+++ src/views/examples/UseContext.jsx
@@ -1,15 +1,19 @@
 import { useContext } from 'react'
+
 import PageTitle from '../../components/layout/PageTitle'
+import SectionTitle from '../../components/layout/SectionTitle'
 import { DataContext } from '../../data/DataContext'
+import { AppContext } from '../../data/Store'
 
 const UseContext = (props) => {
 
-    const context = useContext(DataContext)
-
+    const { state, setState } = useContext(DataContext)
+    const { number, text, setNumber } = useContext(AppContext)
+    
     const addNumber = delta => {
-        context.setState({
-            ...context.state,                      // pega todos os atributos de state
-            number: context.state.number + delta   // alterando apenas o atributo number
+        setState({
+            ...state,                      // pega todos os atributos de state
+            number: state.number + delta   // alterando apenas o atributo number
         })
     }
 
@@ -19,14 +23,28 @@ const UseContext = (props) => {
                 title="Hook UseContext"
                 subtitle="Aceita um objeto de contexto e retorna o valor atual do contexto!"
             />
+
+            <SectionTitle title="Exercício #01" />
             <div className="center">
-                <span className="text">{ context.state.text }</span>
-                <span className="text">{ context.state.number }</span>
+                <span className="text">{ state.text }</span>
+                <span className="text">{ state.number }</span>
                 <div>
                     <button className="btn" onClick={ () => addNumber(-1) }> -1 </button>
                     <button className="btn" onClick={ () => addNumber( 1) }> +1 </button>
                 </div>
             </div>
+
+            <SectionTitle title="Exercício #01" />
+            <div className="center">
+            <span className="text">{ text }</span>
+                <span className="text">{ number }</span>
+                <div>
+                    <button className="btn" onClick={ () => setNumber(number - 1) }> -1 </button>
+                    <button className="btn" onClick={ () => setNumber(number + 1) }> +1 </button>
+                </div>
+            </div>
+
+
         </div>
     )
 }
