c6b302ce9c84be84c5df72333cbbab80cd94dd7b
Celso Vitorino <vitorino.celso@gmail.com>
2024-03-23 23:49:47 -0300

    Hooks Personalizados com exemplo de fetch em url

--- src/views/examples/UseCustom.jsx
+++ src/views/examples/UseCustom.jsx
@@ -1,10 +1,17 @@
 import PageTitle from '../../components/layout/PageTitle'
 import SectionTitle from '../../components/layout/SectionTitle'
 import { useCounter } from '../../hooks/useCounter'
+import { useFetch } from '../../hooks/useFetch'
 
 const UseRef = (props) => {
 
-    const [ count, inc, dec ] = useCounter(3)
+    const [ count, inc, dec ] = useCounter(10)
+
+    const url = 'http://files.cod3r.com.br/curso-react/estados.json'
+    const response = useFetch(url)
+
+    const showStates = (states) => 
+            states.map(state => <li>{ state.nome } - { state.sigla }</li>)
 
     return (
         <div className="UseCustom">
@@ -23,6 +30,12 @@ const UseRef = (props) => {
                 </div>
             </div>
 
+            <SectionTitle title="Exercício #02" />
+            <div className="center">
+                <ul>
+                    {!response.loading ? showStates(response.data) : false}
+                </ul>
+            </div>
         </div>
     )
 }
